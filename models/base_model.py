ec5b4e8b777bb802940a03e990f641d060e1e071
mauro079476 <5090@holbertonschool.com>
2022-10-12 08:39:14 -0700

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -11,13 +11,19 @@ class BaseModel:
     class base mode
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ init functions """
-    
-        self.id = str(uuid.uuid4())
 
-        self.created_at = datetime.datetime.now();
-        self.updated_at = datetime.datetime.now();
+        if len(kwargs) == 0:
+            
+            self.id = str(uuid.uuid4())
+
+            self.created_at = datetime.datetime.now();
+            self.updated_at = datetime.datetime.now();
+        else:
+            for key in kwargs:
+                if key is not "__clas__"
+                    setattr(self, key, kwargs[key])
 
     def __str__(self):
         a = f"[{self.__class__.__name__}] ({self.id}) {self.__dict__} "
