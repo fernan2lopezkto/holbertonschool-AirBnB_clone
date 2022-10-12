1e8db04887c03c321246a9538f57ae69840f5471
juan hp <fernan2lopezkto@gmail.com>
2022-10-12 02:02:41 -0300

    try more things

--- models/base_model.py
+++ models/base_model.py
@@ -15,23 +15,27 @@ class BaseModel:
         """ init functions """
     
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.date.fromtimestamp(time.time());
-        self.updated_at = datetime.date.fromtimestamp(time.time());
+
+        self.created_at = datetime.datetime.now();
+        self.updated_at = datetime.datetime.now();
 
     def __str__(self):
-        a = f"[self.__class__.__name__] ({self.id}) {self.__dict__} "
+        a = f"[{self.__class__.__name__}] ({self.id}) {self.__dict__} "
         return a
 
     def save(self):
-        updated_at = datetime.date.fromtimestamp(time.time());
+        self.updated_at = datetime.datetime.now();
 
     def to_dict(self):
-        item_needed = ["id", "created_at", "updated_at"]
-        self_atrib = self.__dict__
-        filt_atrib = {}
 
-        for itm in range(len(self_atrib)):
-            filt_atrib[item_needed[itm]] = getattr(self, item_needed[itm])
+        self_atr_ibuts = self.__dict__
+        jojo = {}
 
-        return filt_atrib
-        
+        for key in self_atr_ibuts.keys():
+            if key == "created_at" or key == "updated_at":
+                a = self_atr_ibuts[key].isoformat()
+                jojo[key] = a
+            else:
+                jojo[key] = self_atr_ibuts[key]
+
+        return jojo
--- models/test_base_model.py
+++ models/test_base_model.py
@@ -1,18 +1,34 @@
 #!/usr/bin/python3
 from base_model import BaseModel
+import time
+
 
 my_model = BaseModel()
 
+print()
 
+print(my_model)
 
+print()
 
-"""
-print(my_model.id)
+print(type(my_model.created_at))
+print(type(my_model.updated_at))
+
 
 print(my_model.created_at)
 
 print(my_model.updated_at)
-"""
+
+
+time_duration = 3
+time.sleep(time_duration)
+
+print()
+
+my_model.save()
+
+print(my_model.created_at)
+print(my_model.updated_at)
 
 print(my_model.to_dict())
 
