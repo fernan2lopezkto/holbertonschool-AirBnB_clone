666833ab835c9769d22a811877b2fbdfd40ce00b
John King <fernan2lopezkto@gmail.com>
2022-10-13 11:04:34 -0300

    ok more cha

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,6 @@
 
 import uuid
 import datetime
-from models import storage
 
 
 class BaseModel:
@@ -11,24 +10,13 @@ class BaseModel:
     class base mode
     """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         """ init functions """
-
-        if len(kwargs) == 0:
             
-            self.id = str(uuid.uuid4())
+        self.id = str(uuid.uuid4())
 
-            self.created_at = datetime.datetime.now();
-            self.updated_at = datetime.datetime.now();
-
-            storage.new
-        else:
-            for key in kwargs:
-                if key != "__class__":
-                    if key == "created_at" or key == "updated_at":
-                        m = kwargs[key]
-                        datetime.datetime.strptime(m, "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(self, key, kwargs[key])
+        self.created_at = datetime.datetime.now();
+        self.updated_at = datetime.datetime.now();
 
     def __str__(self):
         a = f"[{self.__class__.__name__}] ({self.id}) {self.__dict__} "
@@ -36,7 +24,6 @@ class BaseModel:
 
     def save(self):
         self.updated_at = datetime.datetime.now();
-        storage.save()
 
     def to_dict(self):
 
