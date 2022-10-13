e97312ce3bcbe6a9380b4d1f2784dfa732633eb4
John King <fernan2lopezkto@gmail.com>
2022-10-13 10:45:08 -0300

    correct any things

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,7 @@
 """ storage module """
 import os
 import json
+from models.base_model.BaseModel import to_dict
 
 
 class FileStorage:
@@ -27,7 +28,24 @@ class FileStorage:
         serializes __objects to
         the JSON file (path: __file_path)
         """
-        j_string = json.dumps(self.__objects)
+
+        dic_from_obj = {}
+        for key in self.__objects:
+
+            objet_ki = self.__objects[key]
+
+            self_atr_ibuts = objet_ki.__dict__
+
+            for key in self_atr_ibuts.keys():
+                if key == "created_at" or key == "updated_at":
+                    a = self_atr_ibuts[key].isoformat()
+                    objet_ki[key] = a
+                else:
+                    objet_ki[key] = self_atr_ibuts[key]
+                objet_ki["__class__"] = self.__class__.__name__
+
+
+        j_string = json.dumps(dic_from_obj)
 
         with open(self.__file_path, "w") as f:
             return f.write(j_string)
