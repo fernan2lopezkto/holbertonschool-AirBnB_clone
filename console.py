e81439e67c62b4eea099a6bae3378217909a71ee
mauro079476 <5090@holbertonschool.com>
2022-10-14 11:24:41 -0700

    console

---
+++ console.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""interprete"""
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """class HBNB interpreter"""
+    prompt = "(hbnb)"
+
+    def do_quit(self, arg):
+        """func to quit"""
+        return True
+
+    def do_EOF(self,):
+        """end of a file function"""
+        return True
+
+    def emptyline(self):
+        """empty line"""
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
