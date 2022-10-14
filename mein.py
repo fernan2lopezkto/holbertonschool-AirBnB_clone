9c35300f42008f5ef0bdf13754e2b7adb6a1e2e8
John King <fernan2lopezkto@gmail.com>
2022-10-14 12:46:20 -0300

    more coment

--- mein.py
+++ mein.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-from models import storage
-from models.base_model import BaseModel
+import os
+from models.engine.file_storage import FileStorage
 
-all_objs = storage.all()
-print("-- Reloaded objects --")
-for obj_id in all_objs.keys():
-    obj = all_objs[obj_id]
-    print(obj)
+fs = FileStorage()
+print(type(fs.save))
+file_path = "file.json"
+try:
+    file_path = FileStorage._FileStorage__file_path
+except:
+    pass
 
-print("-- Create a new object --")
-my_model = BaseModel()
-print("objto creado")
-my_model.name = "My_First_Model"
-my_model.my_number = 865
-print("se le agrego cosas al objeto")
-my_model.save()
-print("se le hizo un cambio")
-print(my_model)
+try:    
+    os.remove(file_path)
+except:
+    pass
+
+fs.save()
+
+print(os.path.exists(file_path))
+
+try:
+    os.remove(file_path)
+except Exception as e:
+    pass
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -23,21 +23,21 @@ class FileStorage:
     def save(self):
         """serealize
         """
-        print("start save in storage")
         dic_to_json = {}
+        """create a dictionary to serializate"""
+
         for key in self.__objects:
-            print(type(key))
-            print(type(self.__objects[key]))
-            obj_e = self.__objects[key]
-            print(type(obj_e))
-            print(obj_e)
-            dic_to_json[key] = self.__objects
+            """ran in __obgects"""
+            
+            dic_to_json[key] = self.__objects[key]
+            """set dic_to_json with __objets key value"""
 
         j_string = json.dumps(dic_to_json)
+        """serialized to json strings representation"""
 
         with open(self.__file_path, "w") as f:
             f.write(j_string)
-        print("save ok")
+            """write in file"""
 
     def reload(self):
         """deserializes
@@ -45,14 +45,18 @@ class FileStorage:
 
         if os.path.exists(self.__file_path):
             """open fil """
+
             with open(self.__file_path, "r") as f:
                 """write file as r mode"""
+
                 dict_obgets_from_json = json.load(f)
                 """make a dictionary from json file (deserealized)"""
+
                 for itm in dict_obgets_from_json:
                     """itm is a dict in to the dict_obgets_from_json"""
+
                     from models.base_model import BaseModel
                     """import to use BaseModel"""
+
                     self.__objects[itm] = BaseModel(itm)
-                    """set in __obgets any itm BaseModel instances
-                    """
+                    """set in __obgets any itm BaseModel instances"""
