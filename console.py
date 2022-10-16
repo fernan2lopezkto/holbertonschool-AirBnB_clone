17bba7f4a603118eaec8d811487b1ac8e33e8788
juan hp <fernan2lopezkto@gmail.com>
2022-10-16 13:37:37 -0300

    t7 one element

--- console.py
+++ console.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 """interprete"""
 
+from models.base_model import BaseModel
+from models import storage
 import cmd
+
 
 class HBNBCommand(cmd.Cmd):
     """class HBNB interpreter"""
     prompt = "(hbnb)"
+
+    """task 6
+    """
 
     def do_quit(self, arg):
         """func to quit"""
@@ -19,14 +25,23 @@ class HBNBCommand(cmd.Cmd):
         """empty line"""
         pass
 
+    """task 7
+    """
+
+    def do_create(self, base_name):
+        """create Base MOdel instance"""
+        if base_name:
+            if base_name == "BaseModel":
+                created = BaseModel()
+                print(created.id)
+            else:
+                print("** class doesn't exist **")
+        else:
+            print("** class name missing **")
+
+    def do_show(self,):
+        print("helo")
+
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
-
-    def do_create(self, args)
-        """func that creates new instaance"""
-        if args == "BaseModel":
-            new_base = BaseModel()
-            new_base.save()
-            print(new_base.id)
-
-        if class name 
