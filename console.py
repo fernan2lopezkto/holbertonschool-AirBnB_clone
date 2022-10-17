89100d29a39916b008f8b3e1fb7b4cfe0e9829b5
juan hp <fernan2lopezkto@gmail.com>
2022-10-17 00:12:21 -0300

    fix any thing

--- console.py
+++ console.py
@@ -1,33 +1,24 @@
 #!/usr/bin/python3
 """interprete"""
-
 from models.base_model import BaseModel
 from models import storage
 import cmd
-
-
 class HBNBCommand(cmd.Cmd):
     """class HBNB interpreter"""
     prompt = "(hbnb)"
-
     """task 6
     """
-
     def do_quit(self, arg):
         """func to quit"""
         return True
-
     def do_EOF(self,):
         """end of a file function"""
         return True
-
     def emptyline(self):
         """empty line"""
         pass
-
     """task 7
     """
-
     def do_create(self, base_name):
         """create Base MOdel instance"""
         if base_name:
