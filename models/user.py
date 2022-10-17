1a46ee2ba2c3de8ea978e290b6a6841ed258d63d
mauro079476 <5090@holbertonschool.com>
2022-10-17 07:42:45 -0700

    first user

---
+++ models/user.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""interpreter"""
+
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """class user that inherits from BaseModel"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
